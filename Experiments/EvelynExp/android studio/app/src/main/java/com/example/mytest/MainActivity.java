4a345e1c283c47be30d6b62033a427f37a471347
Evelyn <evelyn@mac-air-027.cs.iastate.edu>
2020-01-28 11:54:35 -0600

    trying again

--- Experiments/EvelynExp/android studio/app/src/main/java/com/example/mytest/MainActivity.java
+++ Experiments/EvelynExp/android studio/app/src/main/java/com/example/mytest/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.mytest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {openSecondPage(); }
+        });
+    }
+
+    public void openSecondPage(){
+        Intent intent = new Intent(this, secondPage.class);
+        startActivity(intent);
     }
 }
