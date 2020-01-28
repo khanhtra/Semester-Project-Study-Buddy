49553bac46760d65bab20a4089796485f526b71e
Evelyn <evelyn@mac-air-027.cs.iastate.edu>
2020-01-28 12:16:59 -0600

    button is working

--- Experiments/EvelynExp/android studio/app/src/main/java/com/example/mytest/MainActivity.java
+++ Experiments/EvelynExp/android studio/app/src/main/java/com/example/mytest/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.mytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
 
+    private Button button;
+    private Intent intent;
+    //private Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {openSecondPage(); }
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openSecondPage(){
-        Intent intent = new Intent(this, secondPage.class);
+        intent = new Intent(this, secondPage.class);
         startActivity(intent);
     }
 }
