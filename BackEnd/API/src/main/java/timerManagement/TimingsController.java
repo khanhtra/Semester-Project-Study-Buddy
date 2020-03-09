44375f50b09da4248ace0f438f78ca7bed090f44
vprao <vprao@iastate.edu>
2020-03-09 16:35:54 -0700

    fixes

--- BackEnd/API/src/main/java/timerManagement/TimingsController.java
+++ BackEnd/API/src/main/java/timerManagement/TimingsController.java
@@ -1,6 +1,8 @@
 package timerManagement;
 
 import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TimingsController 
 {
+	@Autowired
 	TimingsService timingsService = new TimingsService();
 	
 	//Adds a start and end time for a study session to the database
