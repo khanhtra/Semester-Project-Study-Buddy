dc1dbbad2c9c0f082371f94da8a052561c7fb432
Leonard <leonard@mac-air-055.cs.iastate.edu>
2020-03-09 12:51:24 -0500

    Timing Service updated

--- BackEnd/API/src/main/java/timerManagement/TimingsService.java
+++ BackEnd/API/src/main/java/timerManagement/TimingsService.java
@@ -1,5 +1,27 @@
 package timerManagement;
 
-public class TimingsService {
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class TimingsService {
+	@Autowired
+	TimingsRepository timingsRepository;
+
+	public TimingsService(){
+
+	}
+
+	public List<Timings> getTimings (String username){
+		List<Timings> timings = new ArrayList<>();
+		timingsRepository.findByOwnerUsername(username).forEach(pets::add);
+		return timings;
+	}
+	
+	
+	public void addTiming(String username, Date startTime, Date endTime) {
+		timingsRepository.save(new Timings(startTime, endTime, username));
+	}
 }
