84dbbdbe2db9b40821e1228a995e3b5cca7a38a2
Leonard Lim <leonardlwx@gmail.com>
2020-03-09 13:59:45 -0500

    last update i promise

--- BackEnd/API/src/main/java/timerManagement/TimingsService.java
+++ BackEnd/API/src/main/java/timerManagement/TimingsService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import UserManagement.User;
 
 @Service
 public class TimingsService {
