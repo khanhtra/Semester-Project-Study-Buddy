44cfe98732f70941732017eb52c887d09cc4f0f9
Evelyn <evelyn@mac-air-027.cs.iastate.edu>
2020-04-15 11:54:42 -0500

    testing friends

--- FrontEnd/StudyBuddy/app/src/main/java/com/example/StudyBuddy/Friends.java
+++ FrontEnd/StudyBuddy/app/src/main/java/com/example/StudyBuddy/Friends.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.StudyBuddy.LocalData.LocalDataStorage;
 import com.example.StudyBuddy.LocalData.User;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 public class Friends extends AppCompatActivity implements addFriendsDialog.addFriendListener{
     private Button addFriends;
@@ -16,12 +25,12 @@ public class Friends extends AppCompatActivity implements addFriendsDialog.addFr
 
     private LocalDataStorage data;
     private User user;
+    private String friendUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
-        String test = "test";
 
         data = new LocalDataStorage(getApplicationContext());
         user = data.getUserData();
@@ -31,8 +40,26 @@ public class Friends extends AppCompatActivity implements addFriendsDialog.addFr
 
         addFriends.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
                 openDialog();
+
+                String URL = "http://coms-309-vb-5.cs.iastate.edu:8080/users";
+
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL + "/" + friendUsername , null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //add friend to user
+                    }},
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Snackbar noUser = Snackbar.make(v, "This user does not exist. Failed to add.", Snackbar.LENGTH_LONG);
+                                noUser.show();
+                            }
+                        });
+
+                queue.add(request);
             }
         });
 
@@ -52,8 +79,11 @@ public class Friends extends AppCompatActivity implements addFriendsDialog.addFr
         addNewFriend.show(getSupportFragmentManager(), "Add friend");
     }
 
-    @Override
-    public void applyUsername(String name) {
 
-    }
+    /**
+     * sets friendUsername to the name given
+     * @param name
+     */
+    @Override
+    public void applyUsername(String name) {friendUsername = name;}
 }
