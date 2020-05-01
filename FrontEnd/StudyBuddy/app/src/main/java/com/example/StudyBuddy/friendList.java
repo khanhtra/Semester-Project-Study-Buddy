bfade48f495580efad1270637e5a06341305e470
Leonard <leonard@mac-air-055.local>
2020-04-30 23:13:22 -0500

    friendlist added

--- FrontEnd/StudyBuddy/app/src/main/java/com/example/StudyBuddy/friendList.java
+++ FrontEnd/StudyBuddy/app/src/main/java/com/example/StudyBuddy/friendList.java
@@ -2,13 +2,130 @@ package com.example.StudyBuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.example.StudyBuddy.LocalData.LocalDataStorage;
+import com.example.StudyBuddy.LocalData.User;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class friendList extends AppCompatActivity {
+
+    private String TAG = PetList.class.getSimpleName();
+    private ListView lv;
+    private LocalDataStorage data;
+    private User user;
+
+    ArrayList<HashMap<String, String>> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+
+        data = new LocalDataStorage(getApplicationContext());
+        user = data.getUserData();
+
+        contactList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+
+        new friendList.GetList().execute();
+    }
+
+    private class GetList extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(friendList.this,"Json Data is downloading",Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            //TODO
+            String url = "--";
+            String jsonStr = sh.makeServiceCall(url);
+            System.out.println(jsonStr);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    //JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray contacts = new JSONArray(jsonStr);
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        JSONObject tp = c.getJSONObject("type");
+                        String name = c.getString("name");
+                        String id = tp.getString("id");
+                        String subtype = tp.getString("subType");
+                        String rarity = tp.getString("rarity");
+
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("name", name);
+                        contact.put("type", id);
+                        contact.put("subtype", subtype);
+                        contact.put("rarity", rarity);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(friendList.this, contactList,
+                    R.layout.list_frienditem, new String[]{ "name"},
+                    new int[]{R.id.email, R.id.mobile});
+            lv.setAdapter(adapter);
+        }
     }
 }
